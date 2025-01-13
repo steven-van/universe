466437049d7240de0ccbcc29e4cde79453a3d61e
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-13 16:37:53 +0100

    fix: separate auth in route, controller, service file

--- back/src/index.js
+++ back/src/index.js
@@ -1,51 +1,35 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
 const port = process.env.PORT || 8000;
 const con = require("../config/connection");
-const cors = require('cors');
-const User = require("../src/models/User");
-const authRoute = require('./routes/auth');
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
-app.use(cors({
-  origin: "http://localhost:5173", 
-  }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(authRoute);
 
+app.use(authRoutes);
 
 try {
   con.authenticate();
-  console.log('Connection has been established successfully.');
-  con.sync({ force: false }) // force: true recrer les tables, attention à l'utiliser en production
-  .then(() => {
-    console.log('Database & tables created!');
-  })
-  .catch((error) => {
-    console.error('Error during sync:', error);
-  });
+  console.log("Connection has been established successfully.");
+  con
+    .sync({ force: false }) // force: true recrer les tables, attention à l'utiliser en production
+    .then(() => {
+      console.log("Database & tables created!");
+    })
+    .catch((error) => {
+      console.error("Error during sync:", error);
+    });
   app.listen(port, () => {
     console.log(`Server app listening on port ${port}`);
   });
 } catch (error) {
-  console.log('Unable to connect to the database:', error);
+  console.log("Unable to connect to the database:", error);
 }
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
-// A utiliser pour la page inscription
-app.post('/users', async (req, res) => {
-  const { name, email, password } = req.body;
-  try {
-    const saltRounds = 10; // Choisi arbitrairement
-    const hashedPassword = await bcrypt.hash(password, saltRounds)
-    const newUser = await User.create({ name, email, password: hashedPassword });
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to create user' });
-  }
-});
