153a6910a4fe6bd7a70d7b5e6919a9239ecece5f
Bastien <bastien@lacour75.com>
2025-01-19 19:59:22 +0100

    fix: getFriends renvoie des user

--- back/src/services/friendService.js
+++ back/src/services/friendService.js
@@ -7,14 +7,21 @@ exports.addFriend = async (userID, friendID) => {
   };
   
 exports.getFriends = async (userID) => {
-    const friends = await Friend.findAll({ where: { userID }, include: [
-        {
-          model: User,
-          as: 'friendUser',
-          attributes: ['userID', 'name', 'email'] // On veut récupérer ces attributs de l'utilisateur
-        }
-      ]});
-    return friends;
+  const friends = await Friend.findAll({
+    where: { userID },
+    attributes: ['friendID'] // On récupère uniquement les friendID
+  });
+
+  const friendIDs = friends.map(friend => friend.friendID);
+
+  const friendUsers = await User.findAll({
+    where: {
+      userID: friendIDs
+    },
+    attributes: ['userID', 'name', 'email'] // On veut récupérer ces attributs de l'ami
+  });
+
+  return friendUsers;
 };
 
 exports.isFriend = async (userID, friendID) => {
