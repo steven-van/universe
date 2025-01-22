f38a6535dffa0938a257c5588f75a82f815a590c
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-22 21:42:20 +0100

    feat: menu context

---
+++ front/src/enums.js
@@ -0,0 +1,6 @@
+export const MENU_ITEMS = {
+    CONVERSATIONS: "conversations",
+    CONTACTS: "contacts",
+    INFOS: "infos",
+    SETTINGS: "settings"
+}
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ChatSection from "../sections/ChatSection";
-import ContactSection from "../sections/ContactSection";
 import Sidebar from "../sections/Sidebar";
+import ConversationSection from "../sections/ConversationSection";
+import ContactSection from "../sections/ContactSection";
+import { MenuContext } from "../contexts/MenuContext";
+import { MENU_ITEMS } from "../enums";
 
 const Home = () => {
+  const [activeItem, setActiveItem] = useState(MENU_ITEMS.CONVERSATIONS);
+
   return (
-    <div
-      className="container bg-white w-full h-full flex flex-row justify-center items-center"
-    >
-        <Sidebar/>
-        <ContactSection/>
-        <ChatSection/>
+    <div className="container bg-white w-full h-full flex flex-row justify-center items-center">
+      <MenuContext.Provider value={[activeItem, setActiveItem]}>
+        <Sidebar />
+        {activeItem === MENU_ITEMS.CONVERSATIONS && <ConversationSection />}
+        {activeItem === MENU_ITEMS.CONTACTS && <ContactSection />}
+        <ChatSection />
+      </MenuContext.Provider>
     </div>
   );
 };
