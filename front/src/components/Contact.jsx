28be043e74413f8ae04ce3b05e507a141451403a
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-22 21:41:00 +0100

    feat: contact component, contact section

---
+++ front/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Avatar } from "@mui/material";
+import profilePic from "../assets/images/profile_picture.png";
+
+const Contact = () => {
+  return (
+    <div className="flex flex-row items-center justify-between py-2.5 px-3.5 rounded-2xl hover:bg-ACA9E721">
+      <div className="flex items-center">
+        <Avatar
+          src={profilePic}
+          alt="Profile Picture"
+          sx={{ width: 60, height: 60, borderRadius: "20px" }}
+        />
+        <p className="font-robotoBold ml-4">John Doe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ front/src/sections/ContactSection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { IconButton } from "@mui/material";
+import { CustomTextField } from "../components/CustomTextField";
+import { Icon } from "@iconify/react";
+import Contact from "../components/Contact";
+
+const ContactSection = () => {
+  return (
+    <div className="h-full flex">
+      <div className="w-full h-full flex flex-col border-r border-D8D8D8">
+        <div className="flex flex-row justify-between items-center space-x-2 p-4">
+          <CustomTextField fullWidth={true} label="Recherche" />
+          <IconButton color="inherit">
+            <Icon
+              icon="solar:add-circle-bold"
+              width="30"
+              height="30"
+              style={{ color: "#645CF4" }}
+            />
+          </IconButton>
+        </div>
+        <div className="flex justify-evenly border-b border-D8D8D8">
+            <button className="text-sm text-8F8F8F hover:text-645CF4 hover:border-b hover:border-645CF4 px-4 py-2 ">Amis</button>
+            <button className="text-sm text-8F8F8F hover:text-645CF4 hover:border-b hover:border-645CF4 px-4 py-2 ">Demandes</button>
+
+        </div>
+        <div className="min-w-80 flex flex-col mt-4 px-4">
+          <Contact/>
+          <Contact/>
+          <Contact/>
+          <Contact/>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactSection;
