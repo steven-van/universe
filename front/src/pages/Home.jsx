fa0a571c38a62dc9c7cb50cb6235236409794a87
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-24 23:32:01 +0100

    fix: rename UserInfoSection > ContactInfoSection

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,31 +1,18 @@
 import React, { useContext } from "react";
-import ChatSection from "../sections/ChatSection";
 import ContactSection from "../sections/ContactSection";
 import ConversationSection from "../sections/ConversationSection";
 import Sidebar from "../sections/Sidebar";
 import { MENU_ITEMS } from "../enums";
-import UserInfoSection from "../sections/UserInfoSection";
 import { MenuContext } from "../contexts/MenuContext";
 
 const Home = () => {
   const [activeItem] = useContext(MenuContext);
 
-
   return (
     <div className="container bg-white w-full h-full flex flex-row justify-center items-center">
       <Sidebar />
-      {activeItem === MENU_ITEMS.CONVERSATIONS && (
-        <>
-          <ConversationSection />
-          <ChatSection />
-        </>
-      )}
-      {activeItem === MENU_ITEMS.CONTACTS && (
-        <>
-          <ContactSection />
-          <UserInfoSection />
-        </>
-      )}
+      {activeItem === MENU_ITEMS.CONVERSATIONS && <ConversationSection />}
+      {activeItem === MENU_ITEMS.CONTACTS && <ContactSection />}
     </div>
   );
 };
