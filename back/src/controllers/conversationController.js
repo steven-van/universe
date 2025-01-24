fe56e37ea9ad7835ea7a640ea59005fa66148ed5
LouisNG41 <louisgiraudel@gmail.com>
2025-01-24 10:03:32 +0100

    feat : geOrCreateConversation

--- back/src/controllers/conversationController.js
+++ back/src/controllers/conversationController.js
@@ -9,3 +9,13 @@ exports.getUserconversations = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
+
+exports.getOrCreateConversation = async (req, res) => {
+    const { user1Id, user2ID } = req.body;
+    try {
+      const conversation = await conversationservice.getOrCreateConversation(user1Id, user2ID);
+      res.status(200).json(conversation);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
