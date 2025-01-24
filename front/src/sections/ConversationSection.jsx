9b47495abccdee906db20f1904e29f070e0f484e
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-24 23:32:55 +0100

    fix: move chatsection into conversation section

--- front/src/sections/ConversationSection.jsx
+++ front/src/sections/ConversationSection.jsx
@@ -3,31 +3,33 @@ import { IconButton } from "@mui/material";
 import { CustomTextField } from "../components/CustomTextField";
 import { Icon } from "@iconify/react";
 import Conversation from "../components/Conversation";
+import ChatSection from "./ChatSection";
 
 const ConversationSection = () => {
   return (
-    <div className="min-w-80 h-full flex">
-      <div className="w-full h-full flex flex-col p-4 border-r border-D8D8D8">
-        <div className="flex flex-row justify-between items-center space-x-2">
-          <CustomTextField fullWidth={true} label="Recherche" />
-          <IconButton color="inherit">
-            <Icon
-              icon="solar:add-circle-bold"
-              width="30"
-              height="30"
-              style={{ color: "#645CF4" }}
-            />
-          </IconButton>
-        </div>
-        <div className="flex flex-col mt-4">
-          <Conversation />
-          <Conversation />
-          <Conversation />
-          
+    <>
+      <div className="min-w-80 h-full flex">
+        <div className="w-full h-full flex flex-col p-4 border-r border-D8D8D8">
+          <div className="flex flex-row justify-between items-center space-x-2">
+            <CustomTextField fullWidth={true} label="Recherche" />
+            <IconButton color="inherit">
+              <Icon
+                icon="solar:add-circle-bold"
+                width="30"
+                height="30"
+                style={{ color: "#645CF4" }}
+              />
+            </IconButton>
+          </div>
+          <div className="flex flex-col mt-4">
+            <Conversation />
+            <Conversation />
+            <Conversation />
+          </div>
         </div>
       </div>
-    </div>
-    
+      <ChatSection />
+    </>
   );
 };
 
