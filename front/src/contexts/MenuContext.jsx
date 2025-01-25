79949324b407d7eb10b5e38eab8ebb171e37a512
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-25 01:10:37 +0100

    fix: rename enums

--- front/src/contexts/MenuContext.jsx
+++ front/src/contexts/MenuContext.jsx
@@ -1,10 +1,10 @@
 import React, { createContext, useState } from "react";
-import { MENU_ITEMS } from "../enums";
+import { SIDEBAR_MENU } from "../enums";
 
 export const MenuContext = createContext();
 
 export const MenuProvider = ({ children }) => {
-  const [activeItem, setActiveItem] = useState(MENU_ITEMS.CONVERSATIONS);
+  const [activeItem, setActiveItem] = useState(SIDEBAR_MENU.CONVERSATIONS);
 
   return (
     <MenuContext.Provider value={[ activeItem, setActiveItem ]}>
