46abfa4c5a8dd92f625f5c0a4acf1faad0715db6
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-01-25 01:11:00 +0100

    feat: separate contacts and contact requests

--- front/src/sections/ContactSection.jsx
+++ front/src/sections/ContactSection.jsx
@@ -7,10 +7,12 @@ import { AuthContext } from "../contexts/AuthProvider";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import ContactInfoSection from "./ContactInfoSection";
+import { CONTACTS_MENU } from "../enums";
 
 const ContactSection = () => {
   const { authToken } = useContext(AuthContext);
   const [contacts, setContacts] = useState([]);
+  const [activeItem, setActiveItem] = useState(CONTACTS_MENU.CONTACTS);
 
   const getUserIdFromToken = (token) => {
     if (token) {
@@ -53,17 +55,32 @@ const ContactSection = () => {
             </IconButton>
           </div>
           <div className="flex justify-evenly border-b border-D8D8D8">
-            <button className="text-sm text-8F8F8F hover:text-645CF4 hover:border-b hover:border-645CF4 px-4 py-2 ">
+            <button
+              onClick={() => setActiveItem(CONTACTS_MENU.CONTACTS)}
+              className={`${
+                activeItem === CONTACTS_MENU.CONTACTS
+                  ? "text-645CF4 border-b-2 border-645CF4"
+                  : "hover:text-645CF4 hover:border-b-2 hover:border-645CF4 text-8F8F8F"
+              } text-sm px-4 py-2`}
+            >
               Amis
             </button>
-            <button className="text-sm text-8F8F8F hover:text-645CF4 hover:border-b hover:border-645CF4 px-4 py-2 ">
+            <button
+              onClick={() => setActiveItem(CONTACTS_MENU.REQUESTS)}
+              className={`${
+                activeItem === CONTACTS_MENU.REQUESTS
+                  ? "text-645CF4 border-b-2 border-645CF4"
+                  : "hover:text-645CF4 hover:border-b-2 hover:border-645CF4 text-8F8F8F"
+              } text-sm px-4 py-2`}
+            >
               Demandes
             </button>
           </div>
           <div className="min-w-80 flex flex-col mt-4 px-4">
-            {contacts.map((contact) => {
-              return <Contact contact={contact} />;
-            })}
+            {activeItem === CONTACTS_MENU.CONTACTS &&
+              contacts.map((contact) => {
+                return <Contact contact={contact} />;
+              })}
           </div>
         </div>
       </div>
